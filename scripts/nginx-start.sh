f592c3320ce0e4c2287c59e89fdcc017288fed3a
Yuvaraj Yadav <48120373+Yuvaraj421@users.noreply.github.com>
2019-06-08 12:35:28 +0530

    Update nginx-start.sh

--- scripts/nginx-start.sh
+++ scripts/nginx-start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo unlink /etc/nginx/sites-enabled/*
 sudo cp /home/ubuntu/chatapp/chatapp /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/chatapp /etc/nginx/sites-enabled/
 sudo nginx -t
