bc2c1e6ec52b518ac3a6e7eff9446130807eba3d
Yuvaraj Yadav <48120373+Yuvaraj421@users.noreply.github.com>
2019-06-08 12:11:37 +0530

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -7,7 +7,7 @@ python3 -m venv env
 source env/bin/activate
 cd /home/ubuntu/chatapp/
 sudo pip3 install -r requirement.txt
-pip3 install -y channels
-pip3 install -y psycopg2-binary
-pip3 install -y django 
+pip3 install channels
+pip3 install psycopg2-binary
+pip3 install django 
 
