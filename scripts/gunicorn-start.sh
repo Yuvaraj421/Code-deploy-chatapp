2404f70de80427cd6e7c0545de306210de50f387
Yuvaraj Yadav <48120373+Yuvaraj421@users.noreply.github.com>
2019-06-08 12:17:46 +0530

    Update gunicorn-start.sh

--- scripts/gunicorn-start.sh
+++ scripts/gunicorn-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /home/ubuntu/Code-deploy-chatapp/gunicorn.service /etc/systemd/system/
+sudo cp /home/ubuntu/code-deploy-chatapp/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
