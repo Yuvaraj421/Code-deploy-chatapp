d7e685af5eb48f72b81a0bea80c942ef06f1e0c6
Yuvaraj Yadav <48120373+Yuvaraj421@users.noreply.github.com>
2019-06-10 17:57:24 +0530

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -10,7 +10,7 @@ sudo pip3 install -r requirement.txt
 pip3 install channels
 pip3 install psycopg2-binary
 pip3 install django 
-sudo apt install postgresql postgresql-contrib
+sudo apt install -y postgresql postgresql-contrib
 pip3 install gunicorn
 sudo apt-get install nginx -y
 
